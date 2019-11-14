d8b6b68a7ec2dc6ac9419514b7bfe054349210cd
Stan Lemon <stanlemon@users.noreply.github.com>
2019-11-13 22:16:10 -0500

    Switch to component for documents

--- js/App.js
+++ js/App.js
@@ -1,6 +1,6 @@
 import {
   Database,
-  withDocument
+  Document
 } from "https://unpkg.com/@stanlemon/react-pouchdb@0.1.3/dist/react-pouchdb.module.js";
 import { Counter } from "./Counter.js";
 
@@ -8,7 +8,9 @@ export function App() {
   return html`
     <${Database}>
       <h1>Counter App</h1>
-      <${withDocument("counter", Counter)} />
+      <${Document} id="counter1">
+        <${Counter} />
+      <//>
     <//>
   `;
 }
